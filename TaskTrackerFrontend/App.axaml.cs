cfc79dbbbe8d10769c545acc99135efe3cb3f29b
Hafiz <hafiz.abdulkareem74@thehutgroup.com>
2024-10-03 00:01:08 +0100

    added in files to git ignore

--- TaskTrackerFrontend/App.axaml.cs
+++ TaskTrackerFrontend/App.axaml.cs
@@ -1,12 +1,18 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
 using TaskTrackerFrontend.ViewModels;
+using TaskTrackerFrontend.Services;
 
 namespace TaskTrackerFrontend
 {
     public partial class App : Application
     {
+        public IServiceProvider ServiceProvider { get; private set; }
+
         public override void Initialize()
         {
             AvaloniaXamlLoader.Load(this);
@@ -16,13 +22,31 @@ namespace TaskTrackerFrontend
         {
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
+                var services = new ServiceCollection();
+
+                ConfigureServices(services);
+
+                ServiceProvider = services.BuildServiceProvider();
+
                 desktop.MainWindow = new MainWindow
                 {
-                    DataContext = new TaskViewModel(),
+                    DataContext = ServiceProvider.GetRequiredService<TaskViewModel>(),
                 };
             }
 
             base.OnFrameworkInitializationCompleted();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            var apiUrl = Environment.GetEnvironmentVariable("API_URL") ?? throw new InvalidOperationException("API_URL environment variable is not set.");
+
+            services.AddHttpClient<ITaskService, TaskService>(client =>
+            {
+                client.BaseAddress = new Uri(apiUrl);
+            });
+
+            services.AddTransient<TaskViewModel>();
         }
     }
 }
